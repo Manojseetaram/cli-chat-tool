2f654d493c35f57003d7567546ed6f949cd4f8fb
Manojseetaram <manojseetaram.artytech@gmail.com>
2026-02-28 16:58:40 +0530

    write function send message and feat message

--- crates/relay/src/routes.rs
+++ crates/relay/src/routes.rs
@@ -1,10 +1,17 @@
 use axum::{Json, extract::Path};
 use common::models::Message;
+use crate::storage::MESSAGE_STORE;
 
-pub async fn send_message(Json(_msg): Json<Message>) -> &'static str {
-    "Message received"
+pub async fn send_message(Json(msg): Json<Message>) -> &'static str {
+    let mut store = MESSAGE_STORE.lock().unwrap();
+    store.entry(msg.room_id.clone())
+        .or_default()
+        .push(msg);
+    "Stored"
 }
 
-pub async fn fetch_messages(Path(_room_id): Path<String>) -> Json<Vec<Message>> {
-    Json(vec![])
+pub async fn fetch_messages(Path(room_id): Path<String>) -> Json<Vec<Message>> {
+    let mut store = MESSAGE_STORE.lock().unwrap();
+    let messages = store.remove(&room_id).unwrap_or_default();
+    Json(messages)
 }
