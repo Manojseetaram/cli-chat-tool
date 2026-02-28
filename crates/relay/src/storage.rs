802bbf38723465c067884c1fd5953cb9e4f3c9b6
Manojseetaram <manojseetaram.artytech@gmail.com>
2026-02-28 16:57:39 +0530

    relay storage

---
+++ crates/relay/src/storage.rs
@@ -0,0 +1,8 @@
+use std::collections::HashMap;
+use std::sync::Mutex;
+use common::models::Message;
+
+lazy_static::lazy_static! {
+    pub static ref MESSAGE_STORE: Mutex<HashMap<String, Vec<Message>>> =
+        Mutex::new(HashMap::new());
+}
